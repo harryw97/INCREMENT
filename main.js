9fde5e72ad6ce14a86c796a96d35085c44cb985c
harryw97 <sportzdoode1997@gmail.com>
2021-09-24 16:40:22 +1000

    autoDig addition
    added extra button to control game loop and make automatic digging incremental

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var gameData = {
   gold: 0,
   goldPerClick: 1,
-  goldPerClickCost: 10
+  goldPerClickCost: 10,
+  autoDig: 2000,
+  autoDigCost: 5,
+  autoDigLevel: 1
 }
 
 function mineGold() {
@@ -13,15 +16,27 @@ function buyGoldPerClick() {
   if (gameData.gold >= gameData.goldPerClickCost) {
     gameData.gold -= gameData.goldPerClickCost
     gameData.goldPerClick += 1
-    gameData.goldPerClickCost *= 2
+    gameData.goldPerClickCost *= 3
     document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
     document.getElementById("perClickUpgrade").innerHTML = "Upgrade Digger (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
   }
 }
 
+function buyAutoDig() {
+  if (gameData.gold >= gameData.autoDigCost) {
+    gameData.gold -= gameData.autoDigCost
+    gameData.autoDig -= gameData.autoDigCost
+    gameData.autoDigCost *= 2
+    gameData.autoDigLevel ++
+    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Digger (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+    document.getElementById("autoDigUpgrade").innerHTML = "Upgrade Auto-Digger (Currently Level " + gameData.autoDigLevel + ") Cost: " + gameData.autoDigCost + " Gold"
+  }
+}
+
 var mainGameLoop = window.setInterval(function() {
   mineGold()
-}, 1000)
+}, gameData.autoDig)
 
 
 var saveGame = JSON.parse(localStorage.getItem('goldMinerSave'))
